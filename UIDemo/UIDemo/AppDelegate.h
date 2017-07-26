7003bc4edd891a53edeea895c46e6b02889939e1
CODEJY <wujy1284@gmail.com>
2017-07-27 00:21:54 +0800

    UIDemo

---
+++ UIDemo/UIDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIDemo
+//
+//  Created by 吴佳雨 on 2017/7/26.
+//  Copyright © 2017年 OOCL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIDemo/UIDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UIDemo
+//
+//  Created by 吴佳雨 on 2017/7/26.
+//  Copyright © 2017年 OOCL. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
